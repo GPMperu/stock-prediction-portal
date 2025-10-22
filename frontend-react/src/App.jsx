1771be94f1272337ebd618f0656213c0d71d02d4
GPM <juanperu2006@gmail.com>
2025-10-22 10:30:21 -0500

    home

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
-
-
+import './assets/css/style.css'
+import Headers from './components/Header.jsx'
+import Footer from './components/Footer'
+import Main from './components/Main.jsx'
 function App() {
 
 
   return (
     <>
-
+    <Headers />
+    <Main  />
+    <Footer />
     </>
   )
 }
---
+++ frontend-react/src/components/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Button from './Button'
+
+const Main = () => {
+  return (
+    <div className='container'>
+        <div className='p-5 text-center bg-light-dark rounded'>
+            <h1 className='text'>Stock Prediction Portal</h1>
+            <p className='text lead'>Predict your stock prices easily using our portal con mahcine learnign y otras vainas</p>            
+            <Button text="Login" class="btn-info" />
+        </div>
+    </div>
+  )
+}
+
+export default Main
